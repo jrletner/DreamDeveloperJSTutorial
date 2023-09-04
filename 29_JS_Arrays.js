1c7fee2a944129d362c436ee37cac22e3c0c8532
jrletner <jrletner@gmail.com>
2023-09-04 13:32:47 -0500

    commit

---
+++ 29_JS_Arrays.js
@@ -0,0 +1,14 @@
+let supers = [
+  "Superman",
+  "Batman",
+  "Flash",
+  "Aquaman",
+  ["good", "evil", "neutral"],
+  12,
+];
+
+//supers[4] = "Wonder Woman";
+//delete supers[2]; // still in the array just comes back as undefined
+
+console.log(supers);
+console.log(supers[4][2]);
