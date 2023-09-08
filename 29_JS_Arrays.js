82c3dbaec2efe395389ec436fad5ecc0ea3e7c08
jrletner <jrletner@gmail.com>
2023-09-08 13:44:53 -0500

    commit

--- 29_JS_Arrays.js
+++ 29_JS_Arrays.js
@@ -10,5 +10,36 @@ let supers = [
 //supers[4] = "Wonder Woman";
 //delete supers[2]; // still in the array just comes back as undefined
 
-console.log(supers);
-console.log(supers[4][2]);
+// console.log(supers);
+// console.log(supers[4][2]);
+
+// supers.forEach(function (value, index, array) {
+//   console.log(`The index of ${value} is ${index}for the array ${array}`);
+// });
+
+// function upperCase(myValue) {
+//   return myValue.toUpperCase();
+// }
+
+// let supersCap = supers.map(upperCase);
+// console.log("New Array: ", supersCap);
+
+myNumbers = [1, 2, 3, 50, 45, 32, 34, 90, 13, 100];
+function double(x) {
+  return x * 2;
+}
+
+function add(total, value, index, array) {
+  return total + value;
+}
+
+let doubleN = myNumbers.map(double);
+console.log(doubleN);
+
+let evenNumbers = myNumbers.filter(function (value, index, array) {
+  return value % 2 === 0;
+});
+console.log(evenNumbers);
+
+let sum = myNumbers.reduce(add);
+console.log(sum);
